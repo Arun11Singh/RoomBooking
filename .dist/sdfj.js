641a04eb59422d1f7106217d38f518a884ee96ed
arun <gorevk11@gmail.com>
2022-05-11 08:50:56 +0530

    first commet

---
+++ .dist/sdfj.js
@@ -0,0 +1,18 @@
+function Fizzs(num){
+	for(let i=1;i<=num;i++){
+
+	
+  if(i%15==0){
+	   return(i)
+  }else if(i%3==0){
+	 return("Buzz") 
+  }else if(i%5==0){
+	 return( "FizzBuzz")
+  }else {
+	 return("Fizz")
+  }
+}
+}
+let num=
+console.log(Fizzs(num))
+
